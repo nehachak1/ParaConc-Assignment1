6e3b6089163ac8ae4b490d33a1845ead70fc6a1d
Neha Chakraborty <nchakrab@ic1-ubt22-5041.intranet.epfl.ch>
2026-03-03 16:35:28 +0100

    Final program A

--- pi.c
+++ pi.c
@@ -41,33 +41,33 @@ double calculate_pi (int num_threads, int samples) {
     if (samples <= 0 || num_threads <= 0) return 0.0;
 
     // how many random points fall inside circle
-    long long inside = 0; // long used since samples can be very large
+    long long points_inside = 0; // long used since samples can be very large
 
     #pragma omp parallel num_threads(num_threads) // create the threads
     { // runs in parallel
-        int tid = omp_get_thread_num();
+        int thread_id = omp_get_thread_num();
 
         // different random sequences per thread
-        rand_gen gen = init_rand((unsigned int)(tid + 1)); 
+        rand_gen generator = init_rand((unsigned int)(thread_id + 1)); 
 
         // splits the loop iterations among threads
                         // each thread keeps a private copy of inside - prevent race conditions
-        #pragma omp for reduction(+:inside) 
+        #pragma omp for reduction(+:points_inside) 
         // each thread runs its assigned chunk of iterations
         for (long long i = 0; i < (long long)samples; i++) {
             // random point inside unit square 
-            double x = next_rand(gen);   
-            double y = next_rand(gen);  
-            // distance from Origin
+            double x = next_rand(generator);   
+            double y = next_rand(generator);  
+            // distance from origin
             double r2 = x * x + y * y;
-            // check if Inside Circle
-            if (r2 <= 1.0) inside++;
+            // check if inside circle
+            if (r2 <= 1.0) points_inside++;
         }
 
         // frees memory for the current thread’s random generator
-        free_rand(gen);
+        free_rand(generator);
     }
 
-    return 4.0 * (double)inside / (double)samples;
+    return 4.0 * (double)points_inside / (double)samples;
 
 }
